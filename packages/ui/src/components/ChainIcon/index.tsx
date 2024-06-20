7b31041afe89256c04acabf924349eda33b244d1
orisalden <taramaoriskykage@gmail.com>
2024-06-20 10:42:42 +0800

    chore: lorenzo chain config

--- packages/ui/src/components/ChainIcon/index.tsx
+++ packages/ui/src/components/ChainIcon/index.tsx
@@ -3,6 +3,10 @@ import useStyles from '@/components/ChainIcon/useStyles';
 import Image, { type ImageProps } from 'next/image';
 import baseIconLight from 'shared-utils/assets/icons/base-light.svg?url';
 import baseLogoLight from 'shared-utils/assets/logos/base-light.svg?url';
+import lorenzoIconLight from 'shared-utils/assets/icons/lorenzo-light.svg?url';
+import lorenzoIconDark from 'shared-utils/assets/icons/lorenzo-dark.svg?url';
+import lorenzoLogLight from 'shared-utils/assets/logos/lorenzo-light.svg?url';
+import lorenzoLogDark from 'shared-utils/assets/logos/lorenzo-dark.svg?url';
 
 interface IconProps extends Omit<ImageProps, 'id' | 'src'> {
   type: 'icon' | 'logo';
@@ -20,6 +24,10 @@ const ChainIcon = ({
   let [iconDark, iconLight] =
     type === 'icon' ? [baseIconLight, baseIconLight] : [baseLogoLight, baseLogoLight];
   switch (chainName) {
+    case 'lorenzo':
+      [iconDark, iconLight] =
+          type === 'icon' ? [lorenzoIconLight, lorenzoIconDark] : [lorenzoLogLight, lorenzoLogDark];
+      break;
     default:
       throw new Error(`chain ${chainName} not supported`);
   }
